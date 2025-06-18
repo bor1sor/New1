8b734096be32cdcb80443a7a25e4842a50b72701
below_v <100206243+bor1sor@users.noreply.github.com>
2025-06-18 13:08:37 +0300

    test (>)111

--- src/main/java/org/example/Greeting.java
+++ src/main/java/org/example/Greeting.java
@@ -7,7 +7,7 @@ public class Greeting {
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + name + "Nice to meet you");
 
         scanner.close();
     }
