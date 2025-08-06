a59314186e4a13473027a1041b0d659b7cc76f76
below_v <100206243+bor1sor@users.noreply.github.com>
2025-08-06 23:50:13 +0300

    budu + budu

--- src/test/java/BuduAuth.java
+++ src/test/java/BuduAuth.java
@@ -45,16 +45,21 @@ public class BuduAuth {
     public void openBuduRuAndLogin() {
         driver.get("https://budu.ru");
 
-        WebElement button = driver.findElement(By.cssSelector(
-                "#header > div > div.header-desktop__search > div > div.header-desktop__actions > div.s-profile-navigation.header-desktop__actions-item > span"
-        ));
-        String backgroundColor = button.getCssValue("background-color");
-        logger.info("Background color of the element: {}", backgroundColor);
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div/div[2]/div/div[2]/div[3]/span"));
+        button.click();
+        WebElement phone = driver.findElement(By.xpath("//*[@id=\"floatingInput\"]"));
+        phone.sendKeys("(901)766-45-11");
+        WebElement authButton = driver.findElement(By.xpath("//*[@id=\"redesign-layout\"]/main/div/div/div/div/div/div[2]/button"));
+        authButton.click();
+        WebElement AuthCode = driver.findElement(By.xpath("//*[@id=\"floatingInput\"]"));
+        AuthCode.sendKeys("0000");
+
     }
 
     @Test
     @Order(2)
     public void fillPersonalData() {
-        // Реализовать заполнение личных данных здесь
+        driver.quit(); // Реализовать заполнение личных данных здесь
     }
+
 }
