1e6188eac0af3348c271be2a42056c40b156d809
below_v <100206243+bor1sor@users.noreply.github.com>
2025-08-21 23:47:12 +0300

    timing dev05

---
+++ src/test/java/BuduTimingStructure.java
@@ -0,0 +1,36 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BuduTimingStructure {
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://dev05.shop.budu.ru/"; // устанавливаем базовую URI
+    }
+
+    /**
+     * Метод для измерения времени выполнения одного запроса.
+     */
+    private long measureExecutionTime(String endpoint) {
+        long startTime = System.currentTimeMillis();
+        given()
+                .when().get(endpoint)
+                .then()
+                .assertThat();
+        // проверка статуса HTTP-кода
+        return System.currentTimeMillis() - startTime;
+    }
+
+    @Test
+    public void testMultipleEndpointsPerformance() {
+        final String[] endpoints = {"/api/v3/public/site/catalog/search/structure",};
+
+        for (String endpoint : endpoints) {
+            long executionTime = measureExecutionTime(endpoint);
+            System.out.printf("Время выполнения %s: %d мс%n", endpoint, executionTime);
+        }
+    }
+}
