610547fcd4a961eefba2567d963b0a22568cc80b
below_w <100206243+bor1sor@users.noreply.github.com>
2025-12-25 18:57:12 +0300

    Update healthTest.java
    upd

--- src/test/java/healthTest.java
+++ src/test/java/healthTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class healthTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://preprod.shop.budu.ru";
+    }
+
+    @Test
+    public void healthEndpointShouldReturnSimpleTextResponse() {
+        given()
+                .log().all()               // Полная трассировка запросов и ответов
+                .when()
+                .get("/health")           // Проверяем эндпоинт /health
+                .then()
+                .statusCode(200)              // Статус-код должен быть 200
+                .body(is("ok"));              // Тело должно содержать ровно "ok"
+    }
 }
