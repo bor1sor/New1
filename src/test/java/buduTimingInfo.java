3c1aac6a2de614a33f7a84ce8f78054ce32788a7
below_v <100206243+bor1sor@users.noreply.github.com>
2026-01-04 17:57:04 +0300

    поменялась структура api на proxy + добавил другую ручку

--- src/test/java/buduTimingInfo.java
+++ src/test/java/buduTimingInfo.java
@@ -9,13 +9,12 @@ public class buduTimingInfo {
         RestAssured.baseURI = "https://preprod.shop.budu.ru";
         long startTime = System.currentTimeMillis();
         given()
-                .when().get("/api/v1/public/user/info") // Выполняем GET-запрос
+                .when().get("/proxy/v3/public/order/appointment/list") // Выполняем GET-запрос
                 .then()
-                .assertThat()
-                .statusCode(200);
+                .assertThat();
         long endTime = System.currentTimeMillis(); // фиксируем конец времени выполнения запроса
         long executionTime = endTime - startTime; // вычисляем общее время выполнения
 
-        System.out.println("Время выполнения запроса /api/v1/public/user/info: " + executionTime + " мс");// Проверяем статус HTTP-код 200 OK (ставим код 500)
+        System.out.println("Время выполнения запроса proxy/v3/public/order/appointment/list: " + executionTime + " мс");// Проверяем статус HTTP-код 200 OK (ставим код 500)
     }
 }
