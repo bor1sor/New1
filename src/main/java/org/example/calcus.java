0f6d2ac1057455afbe04329a08373b17478aded7
below_v <100206243+bor1sor@users.noreply.github.com>
2026-03-15 14:28:42 +0300

    java1

--- src/main/java/org/example/calcus.java
+++ src/main/java/org/example/calcus.java
@@ -6,7 +6,7 @@ public class calcus {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Сколько тебе лет? ");
+        System.out.print("Сколько тебе полных лет? ");
         int age = scanner.nextInt();
 
         System.out.println("Через 5 лет тебе будет " + (age + 5) + " лет!");
